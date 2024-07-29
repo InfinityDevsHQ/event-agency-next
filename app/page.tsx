f1d7f6e0429872d79608bdb3debe993f42979ea9
sheraz4196 <sheraztoor321@gmail.com>
2024-07-29 14:22:53 +0500

    feat: featured section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/homepage/hero";
+import PopularCities from "@/components/homepage/popular-cities";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <PopularCities />
     </>
   );
 }
---
+++ components/homepage/popular-cities.tsx
@@ -0,0 +1,14 @@
+export default function PopularCities() {
+  return (
+    <section className="py-24 px-12">
+      <div>
+        <div className="flex flex-col gap-5 text-secondary-foreground">
+          <h2 className="font-bold text-5xl">Popular cities</h2>
+          <p className="font-medium text-2xl">
+            Book these events for a close-up look at the city.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
