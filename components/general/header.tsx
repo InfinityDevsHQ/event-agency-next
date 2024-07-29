180735f9783434028b3295cbc42dc7e61b4a9f36
sheraz4196 <sheraztoor321@gmail.com>
2024-07-29 14:00:08 +0500

    feat: site logo

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -1,3 +1,13 @@
+import LogoSvg from "../svgs/logo-svg";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="w-full">
+      <div className="max-w-screen-2xl fixed top-0 w-full mx-auto bg-red-400 py-5 px-12 flex items-center justify-between">
+        <span>
+          <LogoSvg />
+        </span>
+      </div>
+    </header>
+  );
 }
