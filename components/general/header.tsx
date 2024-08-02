3aae2b15c67072b71ffedb9dd90e4cd1a7563948
Danish <danishasifdev@gmail.com>
2024-08-02 13:27:59 +0500

    feat: added nav functionality

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -4,11 +4,13 @@ import LogoSvg from "../svgs/logo-svg";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { MenuIcon, XIcon } from "lucide-react";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   return (
-    <header className="w-full">
+    <header className="w-full relative">
       <div
         id="header"
         className="max-w-screen-2xl h-20 fixed top-0 w-full mx-auto py-5 px-4 md:px-12 flex items-center justify-between"
@@ -39,8 +41,45 @@ export default function Header() {
         >
           Create Event
         </Link>
-        {/* <MenuIcon color="white" className="mt-2" /> */}
-        {/* <XIcon color="white" className="mt-2" /> */}
+        {/* Mobile Nav */}
+        <button
+          className="mt-5 md:mt-0 md:hidden"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <XIcon color="white" className="" />
+          ) : (
+            <MenuIcon color="white" className="" />
+          )}
+        </button>
+      </div>
+
+      <div
+        className={`w-full pt-6 py-8 bg-black/80 text-white md:hidden h-dvh mt-24 ${
+          isOpen ? "absolute" : "hidden"
+        }`}
+      >
+        <ul className="flex flex-col pl-8 gap-10 justify-between">
+          {NavLinks.map((link, index) => (
+            <li key={index}>
+              <Link
+                href={link.link}
+                className="mb-1.5 hover:text-secondary pb-1 font-semibold uppercase hover:text-inverted transition duration-300 ease-in-out"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <Link
+              href="/"
+              className="px-6 py-2 bg-secondary rounded-md text-xs font-bold hover:bg-secondary/90 hover:text-white transform duration-300 ease-in-out"
+            >
+              Create Event
+            </Link>
+          </li>
+        </ul>
       </div>
     </header>
   );
