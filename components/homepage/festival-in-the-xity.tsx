8c666fe76b5d89cc1f3e338df9945f4800fbd654
sheraz4196 <sheraztoor321@gmail.com>
2024-08-08 13:02:40 +0500

    fix: section height

--- components/homepage/festival-in-the-xity.tsx
+++ components/homepage/festival-in-the-xity.tsx
@@ -8,7 +8,7 @@ export default function FestivalInTheCity() {
         backgroundImage: `linear-gradient(to right bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), url('/assets/wallpaper-2.png')`,
       }}
     >
-      <div className="h-[485px] md:p-16 lg:p-24 flex flex-col items-center justify-center md:items-end md:justify-end gap-5">
+      <div className="h-[495px] md:p-16 lg:p-24 flex flex-col items-center justify-center md:items-end md:justify-end gap-5">
         <h3 className="uppercase font-bold text-4xl md:text-5xl lg:text-6xl">
           FESTIVAL IN THE <br /> CITY
         </h3>
